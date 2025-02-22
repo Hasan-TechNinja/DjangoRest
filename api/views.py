d42efe259f5c444be0c7895360b1390030ee0671
Hasan-TechNinja <hasantechninja@gmail.com>
2025-02-22 20:19:12 +0600

    post method complete

--- api/views.py
+++ api/views.py
@@ -9,9 +9,17 @@ from rest_framework import status
 # Create your views here.
 
 
-@api_view(['GET'])
+@api_view(['GET', 'POST'])
 def StudentView(request):
     if request.method == 'GET':
         student = Student.objects.all()
         serializer = StudentSerializer(student, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
+    
+    elif request.method == 'POST':
+        serializer = StudentSerializer(data = request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        else:
+            return Response(serializer.errors)
