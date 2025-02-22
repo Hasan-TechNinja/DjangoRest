69e3a7a298c44813b087c4875d4dfdb5faa5b637
Hasan-TechNinja <hasantechninja@gmail.com>
2025-02-22 13:24:10 +0600

    get data complete

--- api/views.py
+++ api/views.py
@@ -1,10 +1,17 @@
 from django.shortcuts import render
-from students.models import Students
+from students.models import Student
+from . serializers import StudentSerializer
 from django.http import JsonResponse
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
 
 # Create your views here.
 
-def StudentView(request):
-    students = Students.objects.all()
 
-    return JsonResponse(students, safe=False)
+@api_view(['GET'])
+def StudentView(request):
+    if request.method == 'GET':
+        student = Student.objects.all()
+        serializer = StudentSerializer(student, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
--- students/models.py
+++ students/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 
-class Students(models.Model):
+class Student(models.Model):
     student_id = models.CharField(max_length=100)
     name = models.CharField(max_length=200)
     branch = models.CharField(max_length=200)
